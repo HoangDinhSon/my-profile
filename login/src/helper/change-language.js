309e59a03c62053e2c5a4c0fdc713a67ea9391e0
nguyenthithuky <nguyenthithuky151069@gmail.com>
2024-03-10 03:04:05 +0700

    feat: ui lang and add file change language

---
+++ login/src/helper/change-language.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * 1. make object have English and Viet Namese 
+ * 2. get các elemnt need change 
+ * 3. check value of option 
+ * 4. make ui have choosen 
+ * */
+const ENGLISH = {
+    log_in: "Log in",
+}
+const VIETNAM = {
+    log_in: "Đăng nhập"
+}
+const headerLogin = document.getElementById("headerLogin");
+const languageChosen = document.getElementById("language");
+console.log("languageChosen: ", languageChosen.value);
+
+function changeLanguage() {
+    switch (languageChosen.value) {
+        case "ENGLISH":
+            headerLogin.textContent = ENGLISH.log_in
+            break;
+        case "VIETNAM":
+            headerLogin.textContent = VIETNAM.log_in
+            break;
+        default:
+            console.log("ONLY run when not english and vietnam");
+            break;
+    }
+}
+languageChosen.addEventListener("change",changeLanguage)
+
+
